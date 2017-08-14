0f5c40a6baf6791c7b5581329ed1cbb76e0d55e6
Daniel Clain <daniel.clain@MAC000352.local>
2017-08-14 18:09:50 +0100

    trying to get heroku working

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const compression = require('compression');
+const path = require('path');
 const app = express();
+
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
@@ -7,3 +10,11 @@ app.use(express.static(__dirname + '/dist'));
 // Heroku port
 app.listen(process.env.PORT || 8080);
 console.log('express server')
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.use(compression());
