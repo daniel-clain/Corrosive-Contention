7632807692575081a20a83ce4531a5e688f6167c
Daniel Clain <daniel.clain@hotmail.com>
2017-10-28 13:31:08 +0100

    'x'

--- gameHost.js
+++ gameHost.js
@@ -1,9 +1,31 @@
-var app = require('express')();
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// ForceSSL middleware
+app.use(forceSSL());
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+var server = app.listen(8080);
 var io = require('socket.io')(server);
 var fs = require('fs');
 
 
-var server = app.listen(8080);
 var playersCurrentlySearchingForGames = [];
 var numberOfPlayersInEachGame = 1;
 var activeGames = [];
